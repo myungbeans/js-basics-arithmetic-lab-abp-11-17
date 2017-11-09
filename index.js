e497c706783a951e09d3f48a8c8fe45a5009f2b5
Michael Myung <michael.h.myung@gmail.com>
2017-11-09 21:53:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,5 +8,4 @@ You should NOT try to redefine any of the above variables.
 
 // Write your code below this line!
 let newID = oldID+1000000000
-ageIsValid = math.round(currentAge)
-number.is.integer(ageIsValid)
+ageIsValid = number.is.integer(currentAge)
