688ec11d6048c52ac616414387a329e7a0d98726
Michael Myung <michael.h.myung@gmail.com>
2017-11-09 22:09:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,4 @@ let newID = oldID+1000000000
 let ageIsValid = Number.isInteger(currentAge)
 let randomNumber = Math.random(0,20)
 let randomInteger = Math.round(randomNumber)
+let randomUserID = randomInteger+1000000000
